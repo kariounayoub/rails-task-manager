0a8b6690220d04a44c4fa8cb9da45d2b8a0cf618
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-15 15:49:07 +0200

    exo1 finally done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,14 +2,41 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
   def new
-    @task = Task.new()
+    @task = Task.new
   end
+
   def create
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+    redirect_to task_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
